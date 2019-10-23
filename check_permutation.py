3561c14e2335da81b1aec14d655e28862edf00c4
kirega <joseph.mutiga@gmail.com>
2019-10-23 12:34:08 +0300

    Check Permutation Working

--- check_permutation.py
+++ check_permutation.py
@@ -2,9 +2,23 @@
 Given two strings,write a method to decide if one is a permutation of the
 other.
 """
+from collections import defaultdict
+
+
 def check_permutation(a, b):
-    b_map = {}
+    b_map = defaultdict(int)
     if len(a) != len(b):
         return False
     for each in b:
-        
+        b_map[each] += 1
+    for each in a:
+        b_map[each] -= 1
+    for ch in b_map:
+        if b_map[ch] != 0:
+            return False
+    return True
+
+
+a = 'abcddsds'
+b = 'abcddsds'
+print(check_permutation(a, b))
